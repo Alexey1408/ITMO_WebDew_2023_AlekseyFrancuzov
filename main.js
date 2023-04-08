cf258dcb342d8c962e9568572eda5dfc0b021dfe
Alexey1408 <110258341+Alexey1408@users.noreply.github.com>
2023-04-08 03:34:30 +0300

    super simple

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-const DOM = document.getElementById.bind(document);
+// const DOM = document.getElementById.bind(document);
+//
+// const domInpName = DOM("inpName");
+// const domInpSurname = DOM("inpSurname");
+// const domConResult = DOM("conResult");
 
-const domInpName = DOM("inpName");
-const domInpSurname = DOM("inpSurname");
-const domConResult = DOM("conResult");
 
+
+
+// |
+// domInpName.oninput = function (event) {
+//     console.log('onInpNameInput:', { event });
+//     renderFullName();
+// };
+// domInpSurname.oninput = function (event) {
+//     console.log('onInpNameInput:', { event });
+//     renderFullName();
+// };
+//
+//
+// const getFullName = () => `${domInpName.value} ${domInpSurname.value}`;
+//
+// function renderFullName() {
+//     const fullName = getFullName();
+//     console.log("renderFullName:", { fullName });
+//     domConResult.textContent = fullName;
+// }
+//
+//
+// console.log(domInpName, domInpSurname);
+
+// |
 // function  add(a, b = 5) {
 //     const result = a + b;
 //     // console.log('add:', a, b, result);
@@ -16,35 +42,9 @@ const domConResult = DOM("conResult");
 
 
 
-domInpName.oninput = function (event) {
-    console.log('onInpNameInput:', { event });
-    renderFullName();
-};
-domInpSurname.oninput = function (event) {
-    console.log('onInpNameInput:', { event });
-    renderFullName();
-};
-
-
-const getFullName = () => `${domInpName.value} ${domInpSurname.value}`;
-
-function renderFullName() {
-    const fullName = getFullName();
-    console.log("renderFullName:", { fullName });
-    domConResult.textContent = fullName;
-}
-
-
-console.log(domInpName, domInpSurname);
-
-
-
-
-
-
-
+// |
 // event.stopPropagation(); Останавливает Event
-
+//
 // let name = 'Aleksey';
 // let year = 1989;
 // let isHappy = true;
@@ -77,3 +77,56 @@ console.log(domInpName, domInpSurname);
 // domApp.textContent = user1.name;
 //
 // console.log(document.body.first
+
+
+const appendBlock = (block) => document.getElementById("app").appendChild(block);
+const randomRange = (min, max) => Math.floor (Math.random() * (max - min) + min)
+const generateColor = () => {
+    const isNotEmpty = Math.random() > 0.5;
+    if (isNotEmpty) {
+        return "black";
+    }
+    return null;
+}
+const createBlock = (x, y, size, color) => {
+    const result = document.createElement("div");
+    if (color) {
+            result.style.backgroundColor = "color";
+        }
+    console.log(x, y);
+    result.style.width = result.style.height = `${size}px`;
+    result.style.position = "absolute";
+    result.style.left = `${x}px`;
+    result.style.top = `${y}px`;
+    return result;
+};
+
+const BLOCK_SIZE = 50;
+const DIMENSION = 3;
+
+let columns = DIMENSION;
+let rows = columns * 2;
+let xPos = 0;
+let yPos = 0;
+
+
+while (rows-- > 0) {
+    let line = [];
+    xPos = 0;
+    columns = DIMENSION;
+    while (columns-- > 0) {
+        const color = generateColor();
+        const block = createBlock(xPos, yPos, BLOCK_SIZE, color);
+        line.push(color);
+        xPos += BLOCK_SIZE;
+        appendBlock(block);
+    }
+
+    line.reverse().forEach((color) => {
+        const block = createBlock(xPos, yPos, BLOCK_SIZE, color);
+        appendBlock(block);
+        xPos += BLOCK_SIZE;
+    });
+    yPos += BLOCK_SIZE;
+}
+
